14bee4ebb8281b9e4a945fc3916d1b427a0cd2a4
ashokkumawat-autovyn <ashok.kumar@autovyn.in>
2025-06-03 06:36:47 +0530

    git push

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ const __dirname = path.dirname(__filename);
 const swaggerFilePath = path.join(__dirname, 'swagger', 'swagger-output.json');
 const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf-8'));
 const corsOptions = {
-  origin: ['http://localhost:5173',
+  origin: [
+    '*',
+    'http://localhost:5173',
     'https://assuring-javelin-smoothly.ngrok-free.app',
     'http://152.58.70.211:5173',
     'http://[2409:40d4:32:74b6:486a:76ff:fe41:823b]:5173'
