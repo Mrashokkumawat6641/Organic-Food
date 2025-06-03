0f4239ad2acce31a8b214b1fdb60e1512ff4f18b
ashokkumawat-autovyn <ashok.kumar@autovyn.in>
2025-06-03 07:24:02 +0530

    git push

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
 });
 
 // app.use(cors(corsOptions));
-app.use(express.json());
+// app.use(express.json());
 app.use(passport.initialize());
 app.use('/api/auth', authRoutes);
 app.use('/api/image', imageRoutes);
