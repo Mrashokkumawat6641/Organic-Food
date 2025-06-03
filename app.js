d2c315a49ae3e49605655e8647102174eb035281
ashokkumawat-autovyn <ashok.kumar@autovyn.in>
2025-06-03 07:22:01 +0530

    git push

--- app.js
+++ app.js
@@ -20,20 +20,44 @@ const __dirname = path.dirname(__filename);
 
 const swaggerFilePath = path.join(__dirname, 'swagger', 'swagger-output.json');
 const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf-8'));
-const corsOptions = {
-  origin: [
-    '*',
-    'http://localhost:5173',
-    'https://assuring-javelin-smoothly.ngrok-free.app',
-    'http://152.58.70.211:5173',
-    'http://[2409:40d4:32:74b6:486a:76ff:fe41:823b]:5173',
-    'https://organic-food-fronted.vercel.app'
+// const corsOptions = {
+//   origin: [
+//     '*',
+//     'http://localhost:5173',
+//     'https://assuring-javelin-smoothly.ngrok-free.app',
+//     'http://152.58.70.211:5173',
+//     'http://[2409:40d4:32:74b6:486a:76ff:fe41:823b]:5173',
+//     'https://organic-food-fronted.vercel.app'
 
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true,
+//   optionsSuccessStatus: 200
+// };
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://assuring-javelin-smoothly.ngrok-free.app',
+  'http://152.58.70.211:5173',
+  'http://[2409:40d4:32:74b6:486a:76ff:fe41:823b]:5173',
+  'https://organic-food-fronted.vercel.app'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   optionsSuccessStatus: 200
 };
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use((req, res, next) => {
   if (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') {
     bodyParser.json()(req, res, next);
@@ -42,8 +66,8 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(cors(corsOptions));
-// app.use(express.json());
+// app.use(cors(corsOptions));
+app.use(express.json());
 app.use(passport.initialize());
 app.use('/api/auth', authRoutes);
 app.use('/api/image', imageRoutes);
