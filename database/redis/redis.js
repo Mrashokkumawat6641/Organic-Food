6e0e9cb8f1eb70d5acfede9debf7f699c81f304d
ashokkumawat-autovyn <ashok.kumar@autovyn.in>
2025-06-05 17:13:24 +0530

    git push

--- database/redis/redis.js
+++ database/redis/redis.js
@@ -1,28 +1,28 @@
-import { createClient } from 'redis';
-import logger from '../../utils/logger.js';
+// import { createClient } from 'redis';
+// import logger from '../../utils/logger.js';
 
-let client;
+// let client;
 
-export const connectRedis = async () => {
-  try {
-    client = createClient();
-    client.on('error', (err) => {
-      logger.error('Redis Error:', err);
-    });
+// export const connectRedis = async () => {
+//   try {
+//     client = createClient();
+//     client.on('error', (err) => {
+//       logger.error('Redis Error:', err);
+//     });
 
-    await client.connect();
-    logger.info('Redis connected successfully');
-  } catch (error) {
-    logger.error('Redis connection failed:', error);
-    throw error;
-  }
-};
-export const getRedisClient = () => {
-  if (!client || !client.isOpen) {
-    throw new Error('Redis client is not connected. Make sure connectRedis() is called.');
-  }
-  return client;
-};
+//     await client.connect();
+//     logger.info('Redis connected successfully');
+//   } catch (error) {
+//     logger.error('Redis connection failed:', error);
+//     throw error;
+//   }
+// };
+// export const getRedisClient = () => {
+//   if (!client || !client.isOpen) {
+//     throw new Error('Redis client is not connected. Make sure connectRedis() is called.');
+//   }
+//   return client;
+// };
 
 
 // import { createClient } from 'redis';
@@ -63,19 +63,18 @@ export const getRedisClient = () => {
 // };
 
 // database/redis/redis.js
-// import redis from './upstashClient.js';
-// import logger from '../../utils/logger.js';
+import redis from './upstashClient.js';
+import logger from '../../utils/logger.js';
 
-// export const connectRedis = async () => {
-//   try {
-//     // Upstash doesn’t need a real "connect" step, so we just log once.
-//     logger.info('Redis (Upstash) client initialized successfully');
-//   } catch (error) {
-//     logger.error('Redis connection failed:', error);
-//     throw error;
-//   }
-// };
+export const connectRedis = async () => {
+  try {
+    logger.info('Redis (Upstash) client initialized successfully');
+  } catch (error) {
+    logger.error('Redis connection failed:', error);
+    throw error;
+  }
+};
 
-// export const getRedisClient = () => {
-//   return redis;
-// };
+export const getRedisClient = () => {
+  return redis;
+};
