e0bd315b7d152b3f465124b3923c9e1110c45ba9
ashokkumawat-autovyn <ashok.kumar@autovyn.in>
2025-07-13 11:09:15 +0530

    git push

--- api/models/sportsregistrationModels/sportsRegistration.model.js
+++ api/models/sportsregistrationModels/sportsRegistration.model.js
@@ -6,51 +6,61 @@ const mobileUserSchema = new mongoose.Schema({
     },
     coachname: {
         type: String,
-        trim: true
+        trim: true,
+        required: false
     },
     middleName: {
         type: String,
-        trim: true
+        trim: true,
+        required: false
     },
     photo: {
         type: String,
-        trim: true
+        trim: true,
+        required: false
     },
     age: {
         type: Number,
-        trim: true
+        trim: true,
+        required: false
     },
     gender: {
         type: String,
         enum: ['male', 'female', "other"],
-        trim: true
+        trim: true,
+        required: false
     },
     city: {
         type: String,
-        trim: true
+        trim: true,
+        required: false
     },
     state: {
         type: String,
-        trim: true
+        trim: true,
+        required: false
     },
     mobile: {
         type: String,
         match: [
             /^[6-9]\d{9}$/,
             'Please enter a valid 10-digit mobile number'
-        ]
+        ],
+        required: false
     },
     altMobile: {
         type: String,
         match: [
             /^[6-9]\d{9}$/,
             'Please enter a valid 10-digit mobile number'
-        ]
+        ],
+        required: false
 
     },
     selectedGameType: {
         type: String,
         enum: ["indoor", "outdoor"],
+        required: false
     },
     selectedGame: {
         type: String,
@@ -68,31 +78,39 @@ const mobileUserSchema = new mongoose.Schema({
             "Volleyball",
             "Athletics",
         ],
+        required: false
     },
     playerName: {
         type: String,
+        required: false
     },
     playerAge: {
         type: Number,
+        required: false
     },
 
     playerGender: {
         type: String,
         enum: ['male', 'female', "other"],
+        required: false
 
     },
 
     playerCity: {
         type: String,
+        required: false
     },
     playerState: {
         type: String,
+        required: false
     },
     playerPhoto: {
         type: String,
+        required: false
     },
     playerAdharPhoto: {
         type: String,
+        required: false
     },
 }, { timestamps: true });
 
